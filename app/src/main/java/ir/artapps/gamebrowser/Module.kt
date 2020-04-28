05b1627271a5dc31d9a1313a095c76695492d9b4
Navid <navid.kom@gmail.com>
2020-04-29 00:40:16 +0430

    - add Koin DI

---
+++ app/src/main/java/ir/artapps/gamebrowser/Module.kt
@@ -0,0 +1,34 @@
+package ir.artapps.gamebrowser
+
+import androidx.room.Room
+import ir.artapps.gamebrowser.local.AppDatabase
+import ir.artapps.gamebrowser.remote.GamesRemoteDataSource
+import ir.artapps.gamebrowser.remote.GamesRemoteDataSourceImpl
+import ir.artapps.gamebrowser.repo.GamesRepository
+import ir.artapps.gamebrowser.repo.GamesRepositoryImpl
+import ir.artapps.gamebrowser.ui.detail.VenueDetailViewModel
+import ir.artapps.gamebrowser.ui.main.MainViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+/**
+ *   Created by Navid Komijani
+ *   on 29,February,2020
+ */
+
+class Module {
+    val appModule = module {
+        single {
+            Room.databaseBuilder(androidApplication(), AppDatabase::class.java, "database.db")
+                .build()
+        }
+        single { get<AppDatabase>().gamesDAO }
+        single<GamesRemoteDataSource> { GamesRemoteDataSourceImpl() }
+        single<GamesRepository> { GamesRepositoryImpl(get(), get()) }
+
+        viewModel { MainViewModel(get()) }
+        viewModel { VenueDetailViewModel(get()) }
+    }
+}
